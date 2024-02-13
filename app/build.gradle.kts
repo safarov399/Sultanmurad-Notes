f61d935a1019168263eb1d980a8b27f220a30baf
safarov399 <sultanmuradsafarov@gmail.com>
2024-02-13 18:54:01 +0400

    Fixed the current version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 33
         targetSdk = 34
         versionCode = 1
-        versionName = "0.0.2 Beta"
+        versionName = "0.0.3 Beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
